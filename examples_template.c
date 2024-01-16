cb2a8e4f2d58c1c5f1de944fc8687ea399a0c6a7
atomharcos99 <benkovszkylaszlo99@gmail.com>
2024-01-16 22:36:41 +0100

    mégegy teszt

--- examples_template.c
+++ examples_template.c
@@ -9,6 +9,7 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
+   
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
